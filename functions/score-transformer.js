30e2a931758c953808b47d9560e29e774dab433d
Andrew Rob <andrew-c-rob@live.com>
2018-05-03 12:22:49 -0500

    Added comments for scoreTransformer

--- functions/score-transformer.js
+++ functions/score-transformer.js
@@ -1,12 +1,12 @@
-var scoreTransformer = function (list) {
-  var scores = {};
-  for(var i = 0; i < list.length; i++) {
-    let score = list[i][1];
-    let word = list[i][0];
-    if(!(score in scores)) {
-      scores[score] = [];
+var scoreTransformer = function (list) {//function that accepts an array
+  var scores = {};//an empty object called scores is created
+  for(var i = 0; i < list.length; i++) {//loop that iterates through list of scores
+    let score = list[i][1];//initializes score variable to be 2nd element in the element at the current index of array
+    let word = list[i][0];//initializes word variable to be 1st element in the element at the current index of array
+    if(!(score in scores)) {//if the score is not in the scores object
+      scores[score] = [];//the value of the score key is an empty array
     }
-    scores[score].push(word);
+    scores[score].push(word);//otherwise, push a word into the value of the score key in the scores object
   }
-  return scores;
-}
+  return scores;//return scores object
+};
