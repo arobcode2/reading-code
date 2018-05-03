d7842ff655ac6f24912b5efb9842d9c1e66f69d9
Andrew Rob <andrew-c-rob@live.com>
2018-05-03 12:43:29 -0500

    Refactored the comments to scoreTransformer

--- functions/score-transformer.js
+++ functions/score-transformer.js
@@ -1,12 +1,12 @@
-var scoreTransformer = function (list) {//function that accepts an array
+var scoreTransformer = function (list) {//function that accepts an array of arrays
   var scores = {};//an empty object called scores is created
-  for(var i = 0; i < list.length; i++) {//loop that iterates through list of scores
-    let score = list[i][1];//initializes score variable to be 2nd element in the element at the current index of array
-    let word = list[i][0];//initializes word variable to be 1st element in the element at the current index of array
+  for(var i = 0; i < list.length; i++) {//loop that iterates through list of arrays
+    let score = list[i][1];//initializes score variable to be 2nd element in the array at the current index of list
+    let word = list[i][0];//initializes word variable to be 1st element in the array at the current index of list
     if(!(score in scores)) {//if the score is not in the scores object
-      scores[score] = [];//the value of the score key is an empty array
+      scores[score] = [];//creates a new property in the scores object with a key of score and a value of an empty array
     }
-    scores[score].push(word);//otherwise, push a word into the value of the score key in the scores object
+    scores[score].push(word);//push word into the value of score key in the scores object
   }
   return scores;//return scores object
 };
