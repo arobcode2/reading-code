ba696f710013eadb11e91e776e54228dd4503c12
Andrew Rob <andrew-c-rob@live.com>
2018-05-03 12:03:09 -0500

    Added a comment to each line of code examing what each line does

--- functions/distance-to-bun.js
+++ functions/distance-to-bun.js
@@ -1,7 +1,7 @@
-var distanceToBun = function(ingredients, ingredient) {
-  for(var i = 0; i < ingredients.length; i++) {
-    if(ingredients[i] === ingredient) {
-      return Math.min(i, ingredients.length - i);
+var distanceToBun = function(ingredients, ingredient) {//funcation called distanceToBun that accepts an array of ingredientss and 1 ingredient
+  for(var i = 0; i < ingredients.length; i++) {//loop that iterates through ingredients one by one
+    if(ingredients[i] === ingredient) {//checking to see if the ingredient at the current index is equal to the passed in ingredient
+      return Math.min(i, ingredients.length - i);//if the current ingredient is equal to passed in ingredient, returns the lowest index in the ingredients array
     }
   }
-}
+};
